7c86c101a11a6c94dcfa8a5e4b39d115fa140a8e
Blake <blake.duckworth@outlook.com>
2022-10-28 19:31:49 -0400

    add real list fetching

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,20 +1,49 @@
 <template>
     <h2 class="wl-list-title">{{ title }}</h2>
-    <router-link :to="{ name: 'wl-detail', params: { id: item.id } }" class="wl-list-item wl-container" v-for="item in lists" :key="item.id">
+    <router-link :to="{ name: 'wl-detail', params: { id: item.ID } }" class="wl-list-item wl-container" v-for="item in list" :key="item.ID" v-if="list.length > 0">
         <div class="wl-icon theme-primary"><i class="iconoir-bookmark-book"></i></div>
         <div class="wl-content">
             <div>
-                <div class="wl-title">{{ item.name }}</div>
-                <div class="wl-small">Created on {{ item.created }}</div>
+                <div class="wl-title">{{ item.Name }}</div>
+                <div class="wl-small">Created on {{ item.CreatedAt }}</div>
             </div>
-            <div>{{ item.items.length }} Items</div>
+            <div>{{ item.ItemCount }} Items</div>
         </div>
     </router-link>
+    <div v-if="list.length === 0 && list_err === null">
+        <div>You don't have any lists yet. Create a new list to get started!</div>
+    </div>
+    <div v-if="list_err !== null">
+        <div>{{ list_err }}</div>
+    </div>
 </template>
 
 <script>
+    import { ref } from 'vue'
+    import { onMounted } from 'vue'
+
     export default {
-        props: ['title', 'lists']
+        props: ['title', 'list_url'],
+        setup(props) {
+            const list = ref([])
+            const list_err = ref(null)
+
+            onMounted(async () => {
+                try {
+                    let data = await fetch(props.list_url)
+                    if (!data.ok) {
+                        console.log(data)
+                        throw Error('Error loading list: ' + data.status + ' ' + data.statusText)
+                    }
+                    data = await data.json()
+                    list.value = data.Data
+                    console.log(data)
+                } catch(err) {
+                    list_err.value = err.message
+                }
+            })
+            return { list, list_err }
+        }
     }
 </script>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,12 @@
 <template>
-  <List :lists="my_lists" :title="my_wl_title"></List>
-  <br />
-  <List :lists="shared_lists" :title="shared_wl_title"></List>
+      <List :list_url="my_lists_url" :title="my_wl_title"></List>
+      <br />
+      <List :list_url="shared_lists_url" :title="shared_wl_title"></List>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
-
 import { ref } from 'vue'
 import List from '@/components/List.vue'
 
@@ -15,21 +14,13 @@ export default {
   name: 'HomeView',
   components: { List },
   setup() {
-    let my_lists = ref([
-      {"name": "Christmas Wishlist 2022", "items": [{"name": "", "url": "", "price": "", "quantity": "", "notes": ""}], "id": 123, "created": "2022-04-05"},
-      {"name": "Christmas Wishlist 2021", "items": [{"name": "", "url": "", "price": "", "quantity": "", "notes": ""}], "id": 345, "created": "2022-04-05"},
-      {"name": "Christmas Wishlist 2020", "items": [{"name": "", "url": "", "price": "", "quantity": "", "notes": ""}], "id": 456, "created": "2022-04-05"},
-    ])
-    let my_wl_title = ref("My Wishlists")
+    const my_lists_url = ref('http://localhost:9191/api/prot/wishlist')
+    const my_wl_title = ref("My Wishlists")
 
-    let shared_lists = ref([
-      {"name": "Sally's Wishlist 2022", "items": [{"name": "", "url": "", "price": "", "quantity": "", "notes": ""}], "id": 654, "created": "2022-04-05"},
-      {"name": "Mark's Wishlist 2021", "items": [{"name": "", "url": "", "price": "", "quantity": "", "notes": ""}], "id": 765, "created": "2022-04-05"},
-      {"name": "John's Stuff", "items": [{"name": "", "url": "", "price": "", "quantity": "", "notes": ""}], "id": 876, "created": "2022-04-05"},
-    ])
-    let shared_wl_title = ref("Shared with me")
+    const shared_lists_url = ref('http://localhost:9191/api/prot/wishlist')
+    const shared_wl_title = ref("Shared with me")
 
-    return { shared_lists, shared_wl_title, my_lists, my_wl_title }
+    return { my_lists_url, my_wl_title, shared_lists_url, shared_wl_title }
   }
 }
 </script>
